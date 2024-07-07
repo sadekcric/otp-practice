c5d970a77eabfd44b34faf5130e83c4d7eaf802f
sadekcric4 <sadekcric4@gmail.com>
2024-07-07 20:24:53 +0600

    otp generate website done

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const otpInputFunc = () => {
   const inputDivId = document.getElementById("generate_otp_list_id");
+  const storeOtp = document.getElementById("random_otp_id");
+  const conformationMessage = document.getElementById("otp_validation_msg_id");
   let otpCollect = "";
+  let otp = otpGenerator();
+  storeOtp.innerText = `Your OTP is: ${otp}`;
 
   inputDivId.addEventListener("input", (e) => {
     let data = e.target.value;
@@ -8,8 +12,19 @@ const otpInputFunc = () => {
     // OTP Collect form Input Field
     otpCollect = [...inputDivId.children].map((input) => input.value).join("");
 
-    // OTP Input Number Validation Implement
+    // Logic for otp Match
+    if (otp === parseInt(otpCollect, 10)) {
+      conformationMessage.innerText = `Your OTP is Correct! `;
+      conformationMessage.style.color = "#008000";
+    }
+
+    if (otp !== parseInt(otpCollect, 10)) {
+      conformationMessage.innerText = `Invalid OTP`;
+      conformationMessage.style.color = "#ff0000";
+    }
+
     if (isNaN(data)) {
+      // OTP Input Number Validation Implement
       return (e.target.value = "");
     }
 
@@ -24,6 +39,11 @@ const otpInputFunc = () => {
       }
     });
   });
+};
+
+const otpGenerator = () => {
+  const randomNumber = Math.floor(Math.random() * 1000 + 9000);
+  return randomNumber;
 };
 
 otpInputFunc();
